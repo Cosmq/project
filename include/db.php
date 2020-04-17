a5ef2c32f15a061b8ce6e68db884b895d4e024b3
Cosmq <63866475+Cosmq@users.noreply.github.com>
2020-04-17 17:51:34 +0100

    Update db.php

--- include/db.php
+++ include/db.php
@@ -2,7 +2,7 @@
 
 $dbServername = "localhost:3306";
 $dbUsername = "mwakefield1";
-$dbPassword = "weweti";
+$dbPassword = "";
 $dbName = "mwakefield1";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)or die("Error in connection!");
